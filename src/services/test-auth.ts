9891119df7772330081f74a38be91b7ab62a5d27
kwame cody <kwamecody@gmail.com>
2025-12-09 10:29:31 +0000

    [200~git add .

---
+++ src/services/test-auth.ts
@@ -0,0 +1,40 @@
+// src/services/test-auth.ts
+import { createUser } from './user.service';
+import { prisma } from '../lib/prisma';
+import { comparePassword } from '../utils/password';
+
+async function main() {
+  console.log('Testing Auth Security...');
+
+  const email = 'dave@secure.com';
+  const rawPassword = 'super_secret_password';
+
+  await prisma.user.deleteMany({ where: { email } });
+
+  console.log(`1. Creating user Dave with password: "${rawPassword}"`);
+  const dave = await createUser({
+    email: email,
+    username: "Dave secure",
+    password: rawPassword
+  });
+
+  console.log(`2. User Created! ID: ${dave.id}`);
+  console.log(`3. Stored Password in DB: ${dave.password}`);
+
+  if (dave.password === rawPassword) {
+    console.error('FAIL: Password was stored in plain text!');
+  } else if (dave.password.startsWith('$2')) {
+    console.log('PASS: Password is hashed correctly.');
+  }
+
+  console.log('\n4. Testing Login Logic...');
+  const isMatch = await comparePassword(rawPassword, dave.password);
+
+  if (isMatch) {
+    console.log("PASS: 'super_secret_password' matches the hash.");
+  } else {
+    console.error('FAIL: Password comparison failed.');
+  }
+}
+
+main().finally(() => prisma.$disconnect());
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,43 @@
-import { User } from "../schemas/user.schemas";
+import { User } from '../schemas/user.schemas';
+import { prisma } from '../lib/prisma';
+import { hashPassword, comparePassword } from '../utils/password';
+
+interface loginProp {
+  email: string;
+  password: string;
+}
 
 export const createUser = async (data: User) => {
-    console.log("Service: Saving user to DB....", data)
-    return {
-        id: "user_123",
-        ...data,
-        createdAt: new Date()
-    }
-}
+  const hashedPassword = await hashPassword(data.password);
+
+  const user = await prisma.user.create({
+    data: {
+      ...data,
+      password: hashedPassword,
+    },
+  });
+
+  return user;
+};
+
+
+export const login = async (data: loginProp) => {
+ 
+  const user = await prisma.user.findUnique({
+    where: { email: data.email },
+  });
+
+  if (!user) {
+    throw new Error('Invalid email or password');
+  }
+
+  const isValid = await comparePassword(data.password, user.password);
+
+  if (!isValid) {
+    throw new Error('Invalid email or password');
+  }
+
+  const { password, ...userWithoutPassword } = user;
+
+  return userWithoutPassword;
+};
